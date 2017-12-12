094ddfeccfbece96a644285c3085c6d790d45600
marijnschilling <marijnschilling@gmail.com>
2017-12-12 15:29:02 +0100

    Change the text and make tests better

--- Tests/SofaStatusTest.swift
+++ Tests/SofaStatusTest.swift
@@ -17,18 +17,25 @@ class SofaStatusTests: QuickSpec {
                 expect(status.object).to(equal("Marek"))
             }
 
-
             it("creates an attributed string with bold parts for the names") {
+                guard let attributedText = status.attributedText else {
+                    XCTFail("no attributed text set on sofa status")
+                    return
+                }
+
                 let fontAttribute = [NSAttributedStringKey.font: Theme.preferredFootnote()]
 
-                let boldAttribute = status.attributedText?.attributes(at: 0, effectiveRange: nil)
-                expect(boldAttribute?[NSAttributedStringKey.font] as? UIFont).to(equal(Theme.bold(size: 13)))
+                let robertRange = attributedText.string.nsRange(forSubstring: "Robert")
+                let boldAttribute = attributedText.attributes(at: robertRange.location, effectiveRange: nil)
+                expect(boldAttribute[NSAttributedStringKey.font] as? UIFont).to(equal(Theme.bold(size: 13)))
 
-                let boldAttribute2 = status.attributedText?.attributes(at: (status.attributedText?.string.count ?? 14) - 1, effectiveRange: nil)
-                expect(boldAttribute2?[NSAttributedStringKey.font] as? UIFont).to(equal(Theme.bold(size: 13)))
+                let marekRange = attributedText.string.nsRange(forSubstring: "Marek")
+                let boldAttribute2 = attributedText.attributes(at: marekRange.location, effectiveRange: nil)
+                expect(boldAttribute2[NSAttributedStringKey.font] as? UIFont).to(equal(Theme.bold(size: 13)))
 
-                let normalAttributes = status.attributedText?.attributes(at: 8, effectiveRange: nil)
-                expect(normalAttributes?[NSAttributedStringKey.font] as? UIFont).to(equal(Theme.preferredFootnote()))
+                let allRange = attributedText.string.nsRange(forSubstring: "added")
+                let normalAttributes = attributedText.attributes(at: allRange.location, effectiveRange: nil)
+                expect(normalAttributes[NSAttributedStringKey.font] as? UIFont).to(equal(Theme.preferredFootnote()))
             }
         }
     }
